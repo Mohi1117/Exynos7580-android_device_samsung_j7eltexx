76960055ec66027d680b6c3a1c2bc349754c9a58
Mohi <mohiuddinkafre@gmail.com>
2018-02-15 15:19:36 +0530

    update init for 8.1 #1

--- init/Android.mk
+++ init/Android.mk
@@ -5,10 +5,14 @@ LIBINIT_SEC_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := system/core/init
+LOCAL_C_INCLUDES := \
+  system/core/base/include \
+  system/core/init
 LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
 LOCAL_SRC_FILES := init_sec.cpp
 LOCAL_MODULE := libinit_sec
+LOCAL_STATIC_LIBRARIES := libbase
+
 include $(BUILD_STATIC_LIBRARY)
 
 endif
